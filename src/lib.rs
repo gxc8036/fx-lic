9782d18654f484cc175129a1bbf56f0a3c148c35
guxichang <19951799881@163.com>
2023-11-29 16:03:59 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#[no_mangle]
+pub extern "C" fn add(left: i32, right: i32) -> i32 {
+    left + right
+}
+
+#[no_mangle]
+pub extern "C" fn foo() {
+    println!("foo!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
